f30b8e4a099f47cb18ee75865c3d04fc2a659534
anungbisa4 <anung.bisa4@gmail.com>
2017-10-23 11:37:23 +0700

    Inisial Commit Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feBathc4App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the feBathc4App
+ */
+angular.module('feBathc4App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
