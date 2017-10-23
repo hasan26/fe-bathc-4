e9e7fcc38848838cae70a644904786ff99076b34
arifrh0031 <arifrh0031@gmail.com>
2017-10-23 12:40:32 +0700

    initial commit angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feBathc4App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the feBathc4App
+ */
+angular.module('feBathc4App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
